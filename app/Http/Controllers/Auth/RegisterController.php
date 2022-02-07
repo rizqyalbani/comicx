5e13e306017dde9d028579149a343ae185092e25
qyy <rizqyalbani@gmail.com>
2022-02-07 16:06:15 +0800

    Fixing Login Captcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'phone' => ['required', 'string'],
             'password' => ['required', 'string', 'min:8'],
-            'g-recaptcha-response'  => ['required|captcha'],
+            'g-recaptcha-response'  => 'required|captcha',
         ]);
     }
 
